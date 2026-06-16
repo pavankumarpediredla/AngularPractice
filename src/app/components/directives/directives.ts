21d1efc1ab831a230da2e0779f76141b5c043c63
pavankumar.p <pavanpk059@gmail.com>
2026-06-16 14:53:56 +0530

    ng if ngfor

--- src/app/components/directives/directives.ts
+++ src/app/components/directives/directives.ts
@@ -1,33 +1,65 @@
 import { Component } from '@angular/core';
-import { NgIf } from '@angular/common';
+import { NgIf, NgClass, NgForOf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-directives',
-  imports: [NgIf,FormsModule],
+  imports: [NgIf, FormsModule, NgForOf],
   templateUrl: './directives.html',
   styleUrl: './directives.css',
 })
 export class Directives {
+  //*ngIf and @if
   isDivVisible = true;
 
-
-  onShowDiv1(){
-    this.isDivVisible=true;
-
+  onShowDiv1() {
+    this.isDivVisible = true;
   }
 
-  onHideDiv1(){ //logic 1
+  onHideDiv1() {
+    //logic 1
     this.isDivVisible = false;
   }
 
-  toggleDiv1Visibility(){//logic 2
+  toggleDiv1Visibility() {
+    //logic 2
     this.isDivVisible = !this.isDivVisible;
   }
-  
-  hideShowDiv1(isHide:boolean){//logic3
-    this.isDivVisible =isHide;
+
+  hideShowDiv1(isHide: boolean) {
+    //logic3
+    this.isDivVisible = isHide;
   }
 
   hasRefferalCode = false;
+  selectedIdentityProof: string = '';
+  isDiv2Visible: string = 'yes';
+  toggleButton() {
+    this.isDiv2Visible = this.isDiv2Visible == 'yes' ? 'no' : 'yes';
+  }
 
+  textInput: string = '';
+  isVowels: boolean = false;
+
+  onTextChange() {
+    const vowelsList = ['a', 'e', 'i', 'o', 'u'];
+    let isVowelPresent = false;
+    vowelsList.forEach((n) => {
+      if ( this.textInput.includes(n)) {
+        if(isVowelPresent == false ){
+        isVowelPresent = true;
+      }
+      }
+    });
+    this.isVowels = isVowelPresent;
+  }
+  startDayChar = '';
+
+  //ngFor
+  userSelectionView = 'ngFor';
+
+  changeView(view:string){
+    this.userSelectionView = view;
+  }
+
+  studentList = ['Chetan','Rahul','namen','Ankit']
 }
